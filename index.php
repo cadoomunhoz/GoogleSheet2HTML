320adfd658773d5bfc365afc98e90525650ddad3
Cadoo Munhoz <37835658+cadoomunhoz@users.noreply.github.com>
2019-03-01 14:22:26 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>plugin</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<link href="lib/css/style.css" rel="stylesheet" type="text/css" />
+<script async="async" src="lib/js/scripts.js" type="text/javascript"></script>
+
+
+</head>
+
+<body>
+<div id="output"></div>
+</body>
+
+</html>
